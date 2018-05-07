5c3cfed4c516ea4fbdfa72e5ea30056ffcf0565b
Elle Wilson <eleanorwilsonn@gmail.com>
2018-05-07 13:35:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,23 @@
 class Application
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+ def call(env)
+   resp = Rack::Response.new
+   req = Rack::Request.new(env)
 
-    if req.path=="/items"
-      resp.write "#{item.price}\n"
-    elsif
-      req.path=="/items"
-      resp.write "Item not found"
-      resp.status = 400
-    else
-    resp.write "Route not found"
-    resp.status = 404
-    end
-    resp.finish
-  end
+   if req.path.match(/items/)
+     item_name = req.path.split("/items/").last
+     if item =@@items.find{|i| i.name == item_name}
+       resp.write item.price
+     else
+       resp.status = 400
+       resp.write "Item not found"
+     end
+   else
+     resp.status=404
+     resp.write "Route not found"
+   end
+   resp.finish
+ end
+
 end
